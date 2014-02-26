2364ee46fda5dd027912e34638489680f316fab9
Erin Swenson-Healey <erin.swenson.healey@gmail.com>
2014-02-25 17:07:06 -0800

    Bump to 0.0.2 and remove intermediate module

--- lib/barrister-sinatra.rb
+++ lib/barrister-sinatra.rb
@@ -4,38 +4,34 @@ require 'sinatra/base'
 
 module Barrister
 
-  module Sinatra
+  class SinatraContainer
 
-    class Container
+    def initialize(json_path, mount_path, handlers, port=4567, host='localhost')
+      @my_app = ::Sinatra.new do
+        set :bind, host
+        set :port, port
 
-      def initialize(json_path, mount_path, handlers, port=4567, host='localhost')
-        @my_app = ::Sinatra.new do
-          set :bind, host
-          set :port, port
+        contract = Barrister::contract_from_file json_path
+        server   = Barrister::Server.new(contract)
 
-          contract = Barrister::contract_from_file json_path
-          server   = Barrister::Server.new(contract)
+        # register each provided handler
+        handlers.each do |handler_klass|
+          server.add_handler handler_klass.to_s, handler_klass.new
+        end
 
-          # register each provided handler
-          handlers.each do |handler_klass|
-            server.add_handler handler_klass.to_s, handler_klass.new
-          end
+        post mount_path do
+          request.body.rewind
+          resp = server.handle_json(request.body.read)
 
-          post mount_path do
-            request.body.rewind
-            resp = server.handle_json(request.body.read)
-
-            status 200
-            headers 'Content-Type' => 'application/json'
-            resp
-          end
+          status 200
+          headers 'Content-Type' => 'application/json'
+          resp
         end
       end
+    end
 
-      def start
-        @my_app.run!
-      end
-
+    def start
+      @my_app.run!
     end
 
   end
--- lib/barrister-sinatra/version.rb
+++ lib/barrister-sinatra/version.rb
@@ -1,5 +1,5 @@
 module Barrister
   module Sinatra
-    VERSION = "0.0.1"
+    VERSION = "0.0.2"
   end
 end
